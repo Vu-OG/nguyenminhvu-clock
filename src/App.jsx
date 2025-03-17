60a8e4e29e3b74d0d8de8ea74b23cfa3d7cf70f5
takiindev <takin2k5@gmail.com>
2025-03-17 14:04:56 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 import Products from './pages/Products';
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
